6c8001b82adeb560dc249bc4139ef5422ad13099
tukai <snc.jobs@outlook.com>
2017-04-20 17:20:42 +0200

    Kattis Codes

---
+++ Herman.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+
+class Herman
+{
+    public static void main(String[] args)
+    {
+        Scanner inp=new Scanner(System.in);
+        int x=inp.nextInt();
+        System.out.println(Math.PI*Math.pow(x,2));
+        System.out.println(2*Math.pow(x,2));
+    }
+}
